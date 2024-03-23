89a1414d3987ba7d00d20e77302db1b517955bae
anton <antonosland789@gmail.com>
2024-03-22 22:16:31 -0500

    added click event to submit button, not finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,22 @@
 // WHEN I view the footer,
 // THEN I am presented with a link to the developer's portfolio.
 
+const usernameInput = document.querySelector('#username');
+const songTitleInput = document.querySelector('#songTitle');
+const recoPostInput = document.querySelector('#recoPost');
+const submitButton = document.querySelector('#submit');
+
+submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    recoPost = {
+        username: usernameInput.value,
+        songTitle: songTitleInput.value,
+        recoPost: recoPostInput.value
+    }
+
+    console.log()
+
+    window.location.href()
+})
+
